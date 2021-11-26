6e8eba3711679c8171b2cfad4daa57ea2a2b6d73
Lore0905 <lore.castelli4@gmail.com>
2021-11-26 16:00:39 +0100

    esercizio 1 done

--- snack 1/js/script.js
+++ snack 1/js/script.js
@@ -4,3 +4,54 @@
 // proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore 
 // utilizzando destructuring e template literal nel DOM
+
+
+const bike = [
+    {
+        nome: 'red-bike',
+        peso: 15
+    },
+    {
+        nome: 'white-bike',
+        peso: 17
+    },
+    {
+        nome: 'black-bike',
+        peso: 20
+    },
+    {
+        nome: 'green-bike',
+        peso: 10
+    },
+    
+]
+
+
+let currentLightBike = bike[0];
+
+// ciclo tutto l'arrey
+for ( let i = 0; i < bike.length; i++ ){
+
+    const thisBike = bike[i];
+
+    // tengo in considerazione solo la bici con il peso più basso
+    if ( thisBike.peso < currentLightBike.peso ){
+
+        currentLightBike = thisBike;      
+    }
+
+}
+console.log(currentLightBike);
+
+// stampo il risultato nel DOM
+
+const lighterBikeHTML = `
+<div>
+    <h2>la bici più performante che abbiamo, ha un peso di:</h2>
+    <h1>${currentLightBike.peso} kg</h1>
+</div>
+`;
+
+const bikeContainer = document.getElementById('container');
+bikeContainer.innerHTML = lighterBikeHTML;
+
