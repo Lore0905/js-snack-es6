605a2751f0465d441bff70812dd31a438f8f4f3c
Lore0905 <lore.castelli4@gmail.com>
2021-11-29 17:51:10 +0100

    snack 2.1 prima parte fatta

--- snack 2.1/js/script.js
+++ snack 2.1/js/script.js
@@ -1,63 +1,23 @@
-// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse
-// proprietà: nome, punti fatti, falli subiti.
-// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate 
-// a 0.
-// Generare numeri random al posto degli 0 nelle proprietà:
-// Punti fatti e falli subiti.
-// Infine usando la destrutturazione creiamo un nuovo array i cui elementi 
-// contengono solo nomi e falli subiti e stampiamo tutto in console.
+// Snack 2.1
+// Dato un elenco degli studenti di una facoltà, con il totale dei loro voti
 
-const footballTeam = [
-    {
-        nome: 'Juventus',
-        puntiFatti: 0,
-        falliSubiti: 0
-    },
-    {
-        nome: 'Milan',
-        puntiFatti: 0,
-        falliSubiti: 0
-    },
-    {
-        nome: 'Inter',
-        puntiFatti: 0,
-        falliSubiti: 0
-    },
-    {
-        nome: 'Venezia',
-        puntiFatti: 0,
-        falliSubiti: 0
-    },
-    {
-        nome: 'Verona',
-        puntiFatti: 0,
-        falliSubiti: 0
-    }
-];
+let students = [
+    { name: 'Marco', id: 213, grades: 78 },
+    { name: 'Paola', id: 110, grades: 96 },
+    { name: 'Andrea', id: 250, grades: 48 },
+    { name: 'Gaia', id: 145, grades: 74 },
+    { name: 'Luigi', id: 196, grades: 68 },
+    { name: 'Piero', id: 102, grades: 50 },
+    { name: 'Francesca', id: 120, grades: 84 },
+  ];
 
-// creo un nuovo arrey che contiene i punti e i falli subiti
-const newFotballArrey = [];
+// 1- Dobbiamo creare delle targhe con il loro nome in maiuscolo. Ci serve quindi un nuovo array di stringhe. Ogni elemento del nuovo array 
+// sarà il nome dello studente ma con tutte le lettere maiuscole.
 
-for( let i = 0; i < footballTeam.length; i++) {
+const studentsUppercase = students.map((element, index, array) => {
+    return element.name.toUpperCase();
+});
+console.log(studentsUppercase);
 
-    const thisTeam = footballTeam[i];
-
-    footballTeam.puntiFatti = getRndInteger(0, 30);
-    footballTeam.falliSubiti = getRndInteger(10, 20);
-    
-    const {nome} = thisTeam;
-    const {falliSubiti} = footballTeam;
-
-    const copyfootballTeam = {
-        nome, 
-        falliSubiti
-    };
-
-    newFotballArrey.push(copyfootballTeam);
-}
-console.log(newFotballArrey);
-
-
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
+// 2-  Dobbiamo creare un nuovo array con gli studenti che hanno un totale di voti superiore a 70
+// 3-  Dobbiamo creare un nuovo array di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
